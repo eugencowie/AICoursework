d11b998bb3e4516102149d921024197fcc9d3137
Eugén Cowie <eugencowie@users.noreply.github.com>
2017-11-13 12:12:05 +0000

    Stuff

---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    private bool m_isSelected;
+    
+    private void Update()
+    {
+        CheckForSelection();
+        CheckForMoveCommand();
+    }
+
+    private void CheckForSelection()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit) && hit.transform.gameObject == gameObject)
+            {
+                m_isSelected = !m_isSelected;
+                GetComponent<Renderer>().material.color = (m_isSelected ? Color.yellow : Color.white);
+            }
+        }
+    }
+
+    private void CheckForMoveCommand()
+    {
+        if (m_isSelected && Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit) && hit.transform.gameObject != gameObject)
+            {
+                transform.position = hit.point + new Vector3(0, GetComponent<Collider>().bounds.extents.y, 0);
+            }
+        }
+    }
+}
