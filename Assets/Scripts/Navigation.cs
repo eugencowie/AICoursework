ecefb0fd159f5ff7ec58cfecce8dfb73ca0285a5
Eugén Cowie <eugencowie@users.noreply.github.com>
2017-11-27 13:42:27 +0000

    Refactor code

--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -35,7 +35,7 @@ public class Navigation : MonoBehaviour
     public GameObject Sphere;
 
     public Vector2 Size = new Vector2(60, 60);
-    public float NodeSpacing = 2;
+    public float NodeSpacing = 1;
 
     private List<Collider> m_colliders = new List<Collider>();
     private List<Node> m_nodes = new List<Node>();
@@ -43,8 +43,8 @@ public class Navigation : MonoBehaviour
     private void Start()
     {
         m_colliders = GetColliders(ColliderContainer);
-        GenerateNodes();
-        ConnectNodes();
+        m_nodes = GenerateNodes(m_colliders, Sphere, transform, Size, NodeSpacing);
+        ConnectNodes(m_nodes, NodeSpacing);
     }
 
     private void Update()
@@ -54,51 +54,42 @@ public class Navigation : MonoBehaviour
 
     private static List<Collider> GetColliders(GameObject container)
     {
-        List<Collider> colliders = new List<Collider>();
-
-        IEnumerable e = container.transform;
-
-        foreach (Transform transform in e)
-        {
-            Collider collider = transform.gameObject.GetComponent<Collider>();
-            if (collider != null)
-            {
-                colliders.Add(collider);
-            }
-        }
-
-        return colliders;
+        return container.transform.Cast<Transform>().Select(t => t.gameObject.GetComponent<Collider>()).Where(c => c != null).ToList();
     }
 
-    private void GenerateNodes()
+    private static List<Node> GenerateNodes(List<Collider> colliders, GameObject sphere, Transform transform, Vector2 size, float nodeSpacing)
     {
-        // Calculate the offset required to center the grid
-        Vector2 globalOffset = -(Size / 2);
+        List<Node> nodes = new List<Node>();
 
-        for (float y = 0; y < Size.y + 0.01f; y += NodeSpacing)
+        // Calculate the offset required to center the grid
+        Vector2 globalOffset = -(size / 2);
+
+        for (float y = 0; y < size.y + 0.01f; y += nodeSpacing)
         {
-            for (float x = 0; x < Size.x + 0.01f; x += NodeSpacing)
+            for (float x = 0; x < size.x + 0.01f; x += nodeSpacing)
             {
                 Vector2 gridPosition = new Vector2(x, y) + globalOffset;
                 Vector3 position = transform.position + new Vector3(gridPosition.x, 0, gridPosition.y);
 
-                if (!m_colliders.Any(c => c.bounds.Contains(position)))
+                if (!colliders.Any(c => c.bounds.Contains(position)))
                 {
-                    GameObject newNode = Instantiate(Sphere, transform);
+                    GameObject newNode = Instantiate(sphere, transform);
                     newNode.transform.position = position;
                     newNode.SetActive(true);
 
-                    m_nodes.Add(new Node(newNode.transform));
+                    nodes.Add(new Node(newNode.transform));
                 }
             }
         }
+
+        return nodes;
     }
     
-    private void ConnectNodes()
+    private static void ConnectNodes(List<Node> nodes, float nodeSpacing)
     {
-        foreach (Node node in m_nodes)
+        foreach (Node node in nodes)
         {
-            var nearNodes = m_nodes.Where(n => n != node && (n.Position - node.Position).magnitude <= (NodeSpacing * 1.75f));
+            var nearNodes = nodes.Where(n => n != node && (n.Position - node.Position).magnitude <= (nodeSpacing * 1.75f));
             var nearConnections = nearNodes.Select(n => new Connection(n, (n.Position - node.Position).magnitude));
             var validConnections = nearConnections.Where(c => !Physics.Linecast(node.Position, c.Node.Position) && !Physics.Linecast(c.Node.Position, node.Position));
 
