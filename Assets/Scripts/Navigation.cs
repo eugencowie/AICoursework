e14e66c906b38c52f1469c9bdb1e89e658a41588
Eugén Cowie <eugencowie@users.noreply.github.com>
2017-12-04 20:27:48 +0000

    Pathfinding

--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -6,11 +6,15 @@ public class Node
 {
     public Transform Transform;
     public List<Connection> Connections;
+    public Node Parent;
+    public bool Visited;
 
     public Node(Transform transform)
     {
         Transform = transform;
         Connections = new List<Connection>();
+        Parent = null;
+        Visited = false;
     }
 
     public Vector3 Position => Transform.position;
@@ -35,22 +39,14 @@ public class Navigation : MonoBehaviour
 
     public Vector2 Size = new Vector2(60, 60);
     public float NodeSpacing = 1;
-
-    private List<Collider> m_colliders = new List<Collider>();
+    
     private List<Node> m_nodes = new List<Node>();
 
     private void Start()
     {
-        m_colliders = GetColliders(ColliderContainer);
-        m_nodes = GenerateNodes(m_colliders, Sphere, transform, Size, NodeSpacing);
-        ConnectNodes(m_nodes, NodeSpacing);
+        m_nodes = ConnectNodes(GenerateNodes(GetColliders(ColliderContainer), Sphere, transform, Size, NodeSpacing), NodeSpacing);
     }
-
-    private void Update()
-    {
-        DrawDebugLines();
-    }
-
+    
     private static List<Collider> GetColliders(GameObject container)
     {
         return container.transform.Cast<Transform>().Select(t => t.gameObject.GetComponent<Collider>()).Where(c => c != null).ToList();
@@ -84,16 +80,25 @@ public class Navigation : MonoBehaviour
         return nodes;
     }
 
-    private static void ConnectNodes(List<Node> nodes, float nodeSpacing)
+    private static List<Node> ConnectNodes(List<Node> nodes, float nodeSpacing)
     {
-        foreach (Node node in nodes)
+        List<Node> result = nodes.ToList();
+
+        foreach (Node node in result)
         {
-            var nearNodes = nodes.Where(n => n != node && (n.Position - node.Position).magnitude <= (nodeSpacing * 1.75f));
+            var nearNodes = result.Where(n => n != node && (n.Position - node.Position).magnitude <= (nodeSpacing * 1.75f));
             var nearConnections = nearNodes.Select(n => new Connection(n, (n.Position - node.Position).magnitude));
             var validConnections = nearConnections.Where(c => !Physics.Linecast(node.Position, c.Node.Position) && !Physics.Linecast(c.Node.Position, node.Position));
 
             node.Connections.AddRange(validConnections);
         }
+
+        return result;
+    }
+
+    private void Update()
+    {
+        DrawDebugLines();
     }
 
     private void DrawDebugLines()
@@ -105,5 +110,65 @@ public class Navigation : MonoBehaviour
                 Debug.DrawLine(node.Position, connection.Node.Position, Color.green);
             }
         }
+    }
+
+    public static Node GetClosestNode(List<Node> nodes, Vector3 point)
+    {
+        return nodes.OrderBy(n => (point - n.Position).sqrMagnitude).First();
+    }
+
+    public static List<Node> FindPath(List<Node> nodes, Node start, Node end)
+    {
+        foreach (var n in nodes)
+        {
+            n.Parent = null;
+            n.Visited = false;
+        }
+
+        Queue<Node> queue = new Queue<Node>();
+
+        start.Visited = true;
+        queue.Enqueue(start);
+
+        while (queue.Any())
+        {
+            Node current = queue.Dequeue();
+
+            if (current == end)
+            {
+                return ReturnPath(end);
+            }
+
+            foreach (Connection connection in current.Connections)
+            {
+                if (!connection.Node.Visited)
+                {
+                    connection.Node.Visited = true;
+                    connection.Node.Parent = current;
+                    queue.Enqueue(connection.Node);
+                }
+            }
+        }
+
+        return new Queue<Node>(new Node[] { start });
+    }
+
+    private static List<Node> ReturnPath(Node node)
+    {
+        List<Node> foundPath = new List<Node>();
+        if (node != null)
+        {
+            foundPath.Add(node);
+
+            while (node.Parent != null)
+            {
+                foundPath.Add(node.Parent);
+                node = node.Parent;
+            }
+
+            // Reverse the path so the start node is at index 0
+            foundPath.Reverse();
+        }
+        return foundPath;
     }
 }
